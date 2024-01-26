f2be83646eb52b21677ab2ec83b6d58aae306f6d
William Zhao <williezhao123@gmail.com>
2024-01-26 05:16:52 -0800

    Initial Prototype
    - Movement & Dash Controls
    - Food interactables
    - Imported placeholder assets
    - William's Scene - demo scene (DON'T MODIFY!)

---
+++ Global Game Jam 2024/Assets/Scripts/Food.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Item
+{
+    private bool isPickedUp;
+    private Vector2 originalScale;
+    [SerializeField] private float shrinkScale;
+    protected override void Start(){
+        base.Start();
+        originalScale = transform.localScale;
+        isPickedUp = false;
+    } 
+    override protected void Interact()
+    {
+        if (!isPickedUp && isInteractable) {
+            PickUp();
+        } else {
+            Drop();
+        }
+    }
+
+    private void PickUp()
+    {
+        //Make sure player can only pick up one Item at a time
+        if (player.IsCarrying) return;
+        //Pick up food
+        isPickedUp = true;
+        player.IsCarrying = true;
+        transform.SetParent(player.transform, false);
+        transform.localPosition = Vector2.zero;
+        transform.localScale *= shrinkScale;
+    }
+
+    private void Drop()
+    {
+        if(player != null) player.IsCarrying = false;
+        isPickedUp = false;
+        transform.SetParent(null);
+        transform.localScale = originalScale;
+    }
+}
---
+++ Global Game Jam 2024/Assets/Scripts/Item.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Item : MonoBehaviour
+{
+    protected bool isInteractable;
+    protected PlayerController player;
+    protected abstract void Interact();
+    protected virtual void Start()
+    {
+        isInteractable = false;
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact")) {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (player == null) {
+            player = collision.gameObject.GetComponent<PlayerController>();
+        }
+        if (collision.gameObject.tag == "Player") {
+            isInteractable = true;
+        }  
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player") {
+            isInteractable = false;
+        }
+    }
+}
