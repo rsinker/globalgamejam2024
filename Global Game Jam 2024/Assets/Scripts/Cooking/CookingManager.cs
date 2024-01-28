abc9ce61f68ff258fec921c079b8e5190c28443f
William Zhao <32971538+NotSuspicious@users.noreply.github.com>
2024-01-28 10:44:00 -0800

    Imported Sprites. Added Structural Cooking SYstem Code

---
+++ Global Game Jam 2024/Assets/Scripts/Cooking/CookingManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CookingType
+{
+    BLENDED,
+    FRYED,
+    STOVED
+}
+
+public class CookingManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateRecipes()
+    {
+
+    }
+}
