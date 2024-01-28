88e138fb26078006ebde37fad3a5bc1022dec0e5
Roosevelt Sinker <66215548+rsinker@users.noreply.github.com>
2024-01-28 15:54:15 -0800

    Done

--- Global Game Jam 2024/Assets/recipeManager.cs
+++ Global Game Jam 2024/Assets/recipeManager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class recipeManager : MonoBehaviour
 {
-    //public List<Recipe> recipes;
+    public List<Recipe> recipes;
 
-    //private Recipe currentRecipe;
+    private Recipe currentRecipe;
 
     public void PickNewRecipe()
     {
-        //int index = Random.Range(0, recipes.Count);
-        //currentRecipe = recipes[index];
-        //Debug.Log("New Recipe: " + currentRecipe.name);
+        int index = Random.Range(0, recipes.Count);
+        currentRecipe = recipes[index];
+        Debug.Log("New Recipe: " + currentRecipe.name);
     }
 
     public void RecipeMade()
